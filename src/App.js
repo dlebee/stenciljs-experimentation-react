a5cb41b93218748380c7870f986adcfa86d3ebe4
David Lebee <david@poweredsoft.com>
2019-05-30 14:55:10 -0400

    really anoying but something

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { wc } from './util/wc';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component
+{
+  constructor() {
+    super();
+
+    this.myRef = React.createRef();
+
+    this.state = {
+      contact: {
+        firstName: "David",
+        lastName: "Lebee"
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.myRef.current.removeEventListener('sharing');
+  }
+
+  componentDidMount() {
+    this.myRef.current.contact = this.state.contact;
+    this.myRef.current.addEventListener('sharing', e => {
+      alert(e.detail.firstName);
+    });
+  }
+
+  handleFirstNameChanged(e) {
+    this.setState({
+      contact: { ... this.state.contact, firstName: e.target.value }
+    }, () => {
+      this.myRef.current.contact = { ... this.state.contact };
+    })
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <person-card ref={this.myRef}>
+          <div slot="item-header">
+            <p>Some Guy</p>
+            <hr/>
+          </div>
+        </person-card>
+
+        <br />
+        FirstName:
+        <input type="text" value={this.state.contact.firstName} onChange={this.handleFirstNameChanged.bind(this)} />
+
+        <p>{this.state.contact.firstName}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
